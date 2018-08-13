4d1516ab9af0a889f4929e73691194dccd0cdde4
thadler <t.hadler@aol.de>
2018-08-13 16:04:27 +0200

    altering qualitative analysis to work with csv instead of npy files.

--- qualitative_analysis.py
+++ qualitative_analysis.py
@@ -6,7 +6,7 @@ from data_reader import study_ideas_dataset
 
 def load_and_show_similar_to_first_example(texts, matrix_filename):
     texts        = study_ideas_dataset()
-    pairwise_sim = np.load('storage/results/'+matrix_filename)
+    pairwise_sim = np.loadtxt('storage/results/'+matrix_filename)
     sim_tech     = matrix_filename.split('_')[0]
 
     if sim_tech=='lsa':
@@ -15,28 +15,27 @@ def load_and_show_similar_to_first_example(texts, matrix_filename):
         for i in range(len(texts)):
             if sim[i] >= 0.28:
                 print(texts[i], sim[i])
-    
-    if sim_tech=='tfidf':
+    elif sim_tech=='tfidf':
         print('\nSimilarity for TFIDF')
         sim = pairwise_sim[0, :]
         for i in range(len(texts)):
             if sim[i] >= 0.25:
                 print(texts[i], sim[i])
-    
-    if sim_tech=='word2vec':
+    elif sim_tech=='word2vec':
         print('\nSimilarity for WORD2VEC')
         sim = pairwise_sim[0, :]
         for i in range(len(texts)):
             if sim[i] >= 0.905:
                 print(texts[i], sim[i])
+    else:
+        print('similarity technique is not offered')
 
 def show_least_avg_most_similar(texts, matrix_filename):
     texts        = study_ideas_dataset()
-    pairwise_sim = np.load('storage/results/'+matrix_filename)
+    pairwise_sim = np.loadtxt('storage/results/'+matrix_filename)
     sim_tech     = matrix_filename.split('_')[0]
 
     temp_max = 0.0
-    temp_avg = 
     temp_min = 1.0
     for i in range(len(pairwise_sim)):
         for j in range(len(pairwise_sim)):
@@ -47,102 +46,28 @@ def show_least_avg_most_similar(texts, matrix_filename):
             if pairwise_sim[i, j] < temp_min:
                 temp_min = pairwise_sim[i, j]
                 x_min_idx, y_min_idx = i, j
-            if np.abs(pairwise_sim[i, j] - temp_avg) < 0.1**6:
-                x_avg_idx, y_avg_idx = i, j
-    
-
-    print('Very similar')
-    print(x[x_max_idx])
-    print(x[y_max_idx])
-    print(pairwise_sim[x_max_idx, y_max_idx])
+    flat_idx = np.abs(pairwise_sim - np.mean(pairwise_sim)).argmin()
+    x_avg_idx, y_avg_idx = flat_idx//pairwise_sim.shape[0], flat_idx%pairwise_sim.shape[1]
 
     print('Very dissimilar')
-    print(x[x_min_idx])
-    print(x[y_min_idx])
+    print(texts[x_min_idx])
+    print(texts[y_min_idx])
     print(pairwise_sim[x_min_idx, y_min_idx])
 
     print('Average similarity')
-    print(x[x_avg_idx])
-    print(x[y_avg_idx])
+    print(texts[x_avg_idx])
+    print(texts[y_avg_idx])
     print(pairwise_sim[x_avg_idx, y_avg_idx])
-    
 
-
+    print('Very similar')
+    print(texts[x_max_idx])
+    print(texts[y_max_idx])
+    print(pairwise_sim[x_max_idx, y_max_idx])
     
 
 if __name__ == '__main__':
 
     texts           = study_ideas_dataset()
-    matrix_filename = 'tfidf_similarity_blubb.npy'
-    load_and_compare(texts, matrix_filename)
-    quit()
-
-    print('TFIDF EXAMPLES:')
-    temp_max = 0.0
-    temp_avg = 0.5
-    temp_min = 1.0
-    for i in range(len(pairwise_sim)):
-        for j in range(len(pairwise_sim)):
-            if i == j: continue
-            if pairwise_sim[i, j] > temp_max and pairwise_sim[i, j] < 0.999:
-                temp_max = pairwise_sim[i, j]
-                x_max_idx, y_max_idx = i, j
-            if pairwise_sim[i, j] < temp_min:
-                temp_min = pairwise_sim[i, j]
-                x_min_idx, y_min_idx = i, j
-            if np.abs(pairwise_sim[i, j] - temp_avg) < 0.05:
-                x_avg_idx, y_avg_idx = i, j
-
-    print('Very similar')
-    print(x[x_max_idx])
-    print(x[y_max_idx])
-    print(pairwise_sim[x_max_idx, y_max_idx])
-
-    print('Very dissimilar')
-    print(x[x_min_idx])
-    print(x[y_min_idx])
-    print(pairwise_sim[x_min_idx, y_min_idx])
-
-    print('Average similarity')
-    print(x[x_avg_idx])
-    print(x[y_avg_idx])
-    print(pairwise_sim[x_avg_idx, y_avg_idx])
-
-    pairwise_sim = np.load('word2vec_similarity.npy')
-
-    sim_1 = pairwise_sim[0, :]
-    for i in range(len(x)):
-        if sim_1[i] >= 0.85:
-            print(x[i], sim_1[i])
-    quit()
-
-    print('WORD TO VEC EXAMPLES:')
-    temp_max = 0.0
-    temp_avg = 0.5
-    temp_min = 1.0
-    for i in range(len(pairwise_sim)):
-        for j in range(len(pairwise_sim)):
-            if i == j: continue
-            if pairwise_sim[i, j] > temp_max and pairwise_sim[i, j] < 0.999:
-                temp_max = pairwise_sim[i, j]
-                x_max_idx, y_max_idx = i, j
-            if pairwise_sim[i, j] < temp_min:
-                temp_min = pairwise_sim[i, j]
-                x_min_idx, y_min_idx = i, j
-            if np.abs(pairwise_sim[i, j] - temp_avg) < 0.05:
-                x_avg_idx, y_avg_idx = i, j
-
-    print('Very similar')
-    print(x[x_max_idx])
-    print(x[y_max_idx])
-    print(pairwise_sim[x_max_idx, y_max_idx])
-
-    print('Very dissimilar')
-    print(x[x_min_idx])
-    print(x[y_min_idx])
-    print(pairwise_sim[x_min_idx, y_min_idx])
-
-    print('Average similarity')
-    print(x[x_avg_idx])
-    print(x[y_avg_idx])
-    print(pairwise_sim[x_avg_idx, y_avg_idx])
+    matrix_filename = 'word2vec_similarity_blubb.csv'
+    #load_and_show_similar_to_first_example(texts, matrix_filename)
+    show_least_avg_most_similar(texts, matrix_filename)
