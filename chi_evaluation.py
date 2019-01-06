0b7d8c09b2992d86924e67bcab155776aa03dcb1
thadler <t.hadler@aol.de>
2019-01-06 20:01:21 +0100

    chi_evaluation.py works

--- chi_evaluation.py
+++ chi_evaluation.py
@@ -2,24 +2,72 @@ import sys
 from data_reader import *
 from scipy.stats import pearsonr, spearmanr
 
-if __name__ == '__main__':
 
-    script = sys.argv[0]
-    ai_sims  = sys.argv[1] # manually annotated similarities filename 
-    ni_sims  = sys.argv[2] # machine  predicted similarities filename
+def get_ai_txt_2_sims(ai_txts, ai_sims, ni_txt_2_sims):
+    """
+    The ai procedures calculate ALL similarities. Only some are manually 
+    annotated however. So we must select the ai-similarities relevant for the
+    comparison.
+    """
+    
+    txt_2_idx = dict((t.replace('  ',' ').strip(), i) for i,t in enumerate(ai_txts))
+    dictionary = dict()
+    for i, k in enumerate(ni_txt_2_sims):
+        t1, t2 = k[0].replace('  ',' ').strip(), k[1].replace('  ',' ').strip()
+        idx1, idx2 = txt_2_idx[t1], txt_2_idx[t2]
+        dictionary[(t1,t2)] = ai_sims[idx1,idx2]
+        dictionary[(t2,t1)] = ai_sims[idx1,idx2]
+    return dictionary
 
-    if 'ratings' not in ai_sims:
+
+def load_data_chi(chi_nr, manual_fn, automatic_fn):
+    ni_txt_2_sims = load_ni_sims(manual_fn)
+    if chi_nr==1: ai_txts, ai_sims = load_chi19p_C1_complete(automatic_fn)
+    if chi_nr==2: ai_txts, ai_sims = load_chi19p_C2_complete(automatic_fn)
+    if chi_nr==3: ai_txts, ai_sims = load_chi19p_C3_complete(automatic_fn)
+    ai_txt_2_sims = get_ai_txt_2_sims(ai_txts, ai_sims, ni_txt_2_sims)
+    return ni_txt_2_sims, ai_txt_2_sims
+
+
+def get_correlations_chi(ni_txt_2_sims, ai_txt_2_sims):
+    n = len(ni_txt_2_sims.keys())
+    correct_values   = np.zeros(n)
+    predicted_values = np.zeros(n)
+    for i, k in enumerate(sorted(ni_txt_2_sims.keys())):
+        correct_values[i]   = ni_txt_2_sims[k]
+        t1, t2 = k[0].replace('  ',' ').strip(), k[1].replace('  ',' ').strip()
+        predicted_values[i] = ai_txt_2_sims[(t1,t2)]
+    # pearson's correlation coefficient requires: linear relationship, normal distributed deviation 
+    # spearman's correlation coefficient
+    pc = pearsonr(correct_values, predicted_values)[0]
+    sc = spearmanr(correct_values, predicted_values)[0]
+    print("spearman's rank-order correlation coefficient:\n", sc)
+    print("pearson's correlation coefficient:\n",             pc)
+    return pc, sc
+
+    
+def check_input(chi_nr, manual_fn, automatic_fn):
+    if 'ratings' not in manual_fn:
         print("Not the correct filename, doesn't contain 'ratings'")
         quit()
+    if 'c'+str(chi_nr) not in manual_fn:
+        print("Not the correct filename, doesn't contain correct chi_nr")
+        quit()
+
+
+if __name__ == '__main__':
+
+    script       = sys.argv[0]
+    chi_nr       = sys.argv[1] # which chi texts to load
+    manual_fn    = sys.argv[2] # manually annotated similarities filename
+    automatic_fn = sys.argv[3] # machine  predicted similarities filename 
+
+    # assertions
+    check_input(chi_nr, manual_fn, automatic_fn)
 
     # load the data
-    ni_txt_2_sims = load_ni_sims(ni_sims)
-    #ai_sims = np.array(list(map(float, open(ai_sims).readlines())))
+    ni_txt_2_sims, ai_txt_2_sims = load_data_chi(int(chi_nr), manual_fn, automatic_fn)
+
+    # calculate peason and spearman correlation
+    get_correlations_chi(ni_txt_2_sims, ai_txt_2_sims)
     
-    # pearson's correlation coefficient requires:
-    # linear relationship
-    # normal distributed deviation 
-    print("pearson's correlation coefficient:\n", pearsonr(correct_values, predicted_values)[0])
-    
-    # spearman's correlation coefficient
-    print("spearman's rank-order correlation coefficient:\n", spearmanr(correct_values, predicted_values)[0])
--- data_reader.py
+++ data_reader.py
@@ -26,34 +26,51 @@ def load_test_texts_and_similarities():
         sims  = np.array([float(data[i][4]) for i in range(len(data))])
     return texts, sims
 
-def load_chi19ppre_partial_approved():
+
+def load_chi19ppre_partial_approved(sim_prediction_filename=None):
     with open('storage/datasets/chi19ppre-partial-approved.csv') as f:
         data  = csv.reader(f, delimiter=',', quotechar='"')
         texts = [line[2].replace('\n', '') for line in data]
-        sims  = []
+    sims  = []
+    if sim_prediction_filename is not None:    
+        sims = np.loadtxt(sim_prediction_filename)
     return texts, sims
 
-def load_chi19p_C1_complete():
+def load_chi19p_C1_complete(sim_prediction_filename=None):
     with open('storage/datasets/chi19p-C1-complete.csv') as f:
         data  = csv.reader(f, delimiter=',', quotechar='"')
         data  = [line[2] for line in data]
         texts = [line.replace('"','').replace('\n','').replace(',',' ,').replace('.',' .') for line in data[1:]]
-        sims  = []
+    sims  = []
+    if sim_prediction_filename is not None:    
+        sims = np.loadtxt(sim_prediction_filename)
     return texts, sims
 
-def load_chi19p_C3_complete():
+def load_chi19p_C2_complete(sim_prediction_filename=None):
     with open('storage/datasets/chi19p-C3-complete.csv') as f:
         data  = csv.reader(f, delimiter=',', quotechar='"')
         data  = [line[2] for line in data]
         texts = [line.replace('"','').replace('\n','').replace(',',' ,').replace('.',' .') for line in data[1:]]
-        sims  = []
+    sims  = []
+    if sim_prediction_filename is not None:    
+        sims = np.loadtxt(sim_prediction_filename)
+    return texts, sims
+
+def load_chi19p_C3_complete(sim_prediction_filename=None):
+    with open('storage/datasets/chi19p-C3-complete.csv') as f:
+        data  = csv.reader(f, delimiter=',', quotechar='"')
+        data  = [line[2] for line in data]
+        texts = [line.replace('"','').replace('\n','').replace(',',' ,').replace('.',' .') for line in data[1:]]
+    sims  = []
+    if sim_prediction_filename is not None:    
+        sims = np.loadtxt(sim_prediction_filename)
     return texts, sims
 
 def load_ni_sims(fname): # ni = natural intelligence
     """
     returns a dict object with (t1,t2) -> similarity <- (t2,t1)
     """
-    with open('storage/datasets/chi19p-c1-ratings.csv') as f:
+    with open(fname) as f:
         data = csv.reader(f, delimiter=',', quotechar='"')
         data = [line for line in data]
         preprocess = lambda txt: txt.replace('"','').replace('\n','').replace(',',' ,').replace('.',' .')
@@ -71,3 +88,4 @@ if __name__=='__main__':
     load_chi19p_C1_complete()
     load_chi19p_C3_complete()
     load_ni_sims('storage/datasets/chi19p-c1-ratings.csv')
+    load_chi19p_C3_complete('storage/predictions/chi19p-C1-complete/avg_wv_fasttext_sim_cosine.txt')
